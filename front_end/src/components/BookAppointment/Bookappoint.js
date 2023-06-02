bf76d2e29a3e89b2c5df5707b5c51e8f02695f11
Ali Abbas <bsef19a525@pucit.edu.pk>
2023-06-03 02:12:37 +0500

    Home Page Responsiveness, Booking Page, Forget Password Added

--- front_end/src/components/BookAppointment/Bookappoint.js
+++ front_end/src/components/BookAppointment/Bookappoint.js
@@ -16,6 +16,7 @@ function Bookappoint() {
 
   const phoneRef = useRef(null);
   const dateRef = useRef(null);
+  const serviceRef = useRef(null);
 
   const handleBooking = (e) => {
     const { name, value } = e.target;
@@ -33,33 +34,25 @@ function Bookappoint() {
     // Create a new Date object using the date value
     const myDate = new Date(date);
 
+    console.log(myDate);
+
     // Get the current date
     const currentDate = new Date();
 
+    // Set both dates to the same time of day to compare only the dates
+    myDate.setHours(0, 0, 0, 0);
+    currentDate.setHours(0, 0, 0, 0);
+
     // Compare the selected date with the current date
-    if (myDate < currentDate) {
+    if (myDate.getTime() === currentDate.getTime() || myDate > currentDate) {
+      const options = { day: "numeric", month: "long", year: "numeric" };
+      const formattedDate = myDate.toLocaleDateString("en-US", options);
+      console.log("After: " + formattedDate); // Output: "June 22, 2023"
+      return formattedDate;
+    } else {
       return -1;
     }
-    // Format the date using custom formatting
-
-    const options = { day: "numeric", month: "long", year: "numeric" };
-    const formattedDate = myDate.toLocaleDateString("en-US", options);
-
-    console.log("After: " + formattedDate); // Output: "June 22, 2023"
-    return formattedDate;
   };
-
-  // phone Validation Function
-  // const validatePhone = (phone) => {
-  //   const phoneNum = phone.length;
-  //   if (phoneNum < 12 && phoneNum >= 9) {
-  //     return phoneNum;
-  //   } else {
-  //     toast.error("Phone Number Should range from 9 - 11 digits");
-  //     setBookData({ ...bookData, phone: "" });
-  //     .current.focus();
-  //   }
-  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -67,71 +60,79 @@ function Bookappoint() {
 
     const bookingDate = validateDate(date);
 
+    // Phone Number Validity
     if (phone.length < 12 && phone.length >= 9) {
-      if (bookingDate == -1) {
+      // Date Validity --> No Past Date allowed
+      if (bookingDate != -1) {
+        // Service Validity --> Only Alphabets, whitespace character, including spaces, tabs, and line breaks.
+        const alphabetsRegex = /^[A-Za-z\s]+$/;
+        if (alphabetsRegex.test(service)) {
+          try {
+            // Make an API request to your backend server
+            const response = await axios.get(
+              `http://localhost:8000/book/${salonName}`
+            );
+            const data = response.data;
+
+            if (data) {
+              const { email, salonName } = data;
+              console.log("email: " + email);
+              console.log("salonName: " + salonName);
+              toast.loading("Loading...");
+              setTimeout(() => {
+                toast.dismiss();
+              }, 3000);
+              try {
+                // Send the email to the salon using EmailJS
+                const emailResponse = await emailjs.send(
+                  process.env.REACT_APP_EMAILJS_SERVICE_ID,
+                  process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+                  {
+                    salonName: salonName,
+                    salonEmail: email, // Salon's email address retrieved from the backend
+                    fullName,
+                    email: bookData.email,
+                    phone,
+                    date: bookingDate,
+                    service,
+                  },
+                  process.env.REACT_APP_EMAILJS_USER_ID
+                );
+
+                if (emailResponse.status === 200) {
+                  setBookData({
+                    fullName: "",
+                    email: "",
+                    phone: "",
+                    salonName: "",
+                    date: "",
+                    service: "",
+                  });
+                  setTimeout(() => {
+                    toast.success(`Booking request sent to salon`);
+                  }, 3000);
+                }
+                console.log(
+                  "Booking request sent to salon:",
+                  emailResponse.status,
+                  emailResponse.text
+                );
+              } catch (error) {
+                console.error("Error sending email to salon:", error);
+              }
+            }
+          } catch (error) {
+            console.error("Error retrieving salon data:", error.message);
+          }
+        } else {
+          toast.error("Accepting Alphabetic Characters Only");
+          setBookData({ ...bookData, service: "" });
+          serviceRef.current.focus();
+        }
+      } else {
         toast.error("Choose Today or Future Date");
         setBookData({ ...bookData, date: "" });
         dateRef.current.focus();
-      } else {
-        try {
-          // Make an API request to your backend server
-          const response = await axios.get(
-            `http://localhost:8000/book/${salonName}`
-          );
-          const data = response.data;
-
-          if (data) {
-            const { email, salonName } = data;
-            console.log("email: " + email);
-            console.log("salonName: " + salonName);
-            toast.loading("Loading...");
-            setTimeout(() => {
-              toast.dismiss();
-            }, 3000);
-            try {
-              // Send the email to the salon using EmailJS
-              const emailResponse = await emailjs.send(
-                process.env.REACT_APP_EMAILJS_SERVICE_ID,
-                process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-                {
-                  salonName: salonName,
-                  salonEmail: email, // Salon's email address retrieved from the backend
-                  fullName,
-                  email: bookData.email,
-                  phone,
-                  date: bookingDate,
-                  service,
-                },
-                process.env.REACT_APP_EMAILJS_USER_ID
-              );
-
-              if (emailResponse.status === 200) {
-                // setBookData({
-                //   fullName: "",
-                //   email: "",
-                //   phone: "",
-                //   salonName: "",
-                //   date: "",
-                //   service: "",
-                // });
-                setTimeout(() => {
-                  toast.success(
-                    "Thank you for your response! A booking confirmation email will be sent to you shortly."
-                  );
-                }, 2000);
-              }
-              console.log(
-                "Booking request sent to salon:",
-                emailResponse.status,
-                emailResponse.text
-              );
-            } catch (error) {
-              console.error("Error sending email to salon:", error);
-            }
-          }
-        } catch (error) {
-          console.error("Error retrieving salon data:", error.message);
-        }
       }
     } else {
       toast.error("Phone numbers should contain 9 to 11 digits.");
@@ -147,7 +148,7 @@ function Bookappoint() {
         <div className="bookForm">
           <div className="backImg0"></div>
           <div className="form0">
-            <h1 className="title0">Book your Appointment</h1>
+            <h1 className="title0">Book Appointment</h1>
             <form onSubmit={handleSubmit}>
               <div className="name-email0 flex-com0">
                 <input
@@ -155,16 +156,20 @@ function Bookappoint() {
                   value={bookData.fullName}
                   onChange={handleBooking}
                   placeholder="Full Name"
+                  className="margin"
                   name="fullName"
                   required
+                  autoComplete="off"
                 />
                 <input
                   type="email"
                   value={bookData.email}
                   onChange={handleBooking}
                   placeholder="Email Address"
+                  className="margin"
                   name="email"
                   required
+                  autoComplete="off"
                 />
               </div>
               {/* Password & Confirm Password */}
@@ -173,19 +178,23 @@ function Bookappoint() {
                   type="number"
                   value={bookData.phone}
                   onChange={handleBooking}
+                  className="margin"
                   placeholder="Phone no"
                   ref={phoneRef}
                   name="phone"
                   min={10}
                   required
+                  autoComplete="off"
                 />
                 <input
                   type="text"
                   value={bookData.salonName}
                   onChange={handleBooking}
                   placeholder="Salon Name"
+                  className="margin"
                   name="salonName"
                   required
+                  autoComplete="off"
                 />
               </div>
               {/* Salon Name */}
@@ -193,22 +202,27 @@ function Bookappoint() {
                 <input
                   type="date"
                   ref={dateRef}
+                  className="margin"
                   value={bookData.date}
                   onChange={handleBooking}
                   placeholder="Date"
                   name="date"
                   required
+                  autoComplete="off"
                 />
               </div>
               {/* Salon Location */}
               <div className="sal-loc0 sal-common0">
                 <input
                   type="text"
+                  ref={serviceRef}
+                  className="margin"
                   value={bookData.service}
                   onChange={handleBooking}
                   placeholder="Service you want to avail"
                   name="service"
                   required
+                  autoComplete="off"
                 />
               </div>
 
--- front_end/src/components/Login/pass.js
+++ front_end/src/components/Login/pass.js
@@ -1,25 +1,113 @@
+import React, { useState, useRef } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Desktop3 = () => {
+  const [chpassData, setchPassData] = useState({
+    email: "",
+    newpass: "",
+    confpass: "",
+  });
+  const inputRef = useRef(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setchPassData((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, newpass, confpass } = chpassData;
+    //if password and cnfrmpassword are not same
+    if (chpassData.newpass !== chpassData.confpass) {
+      console.log("password and confirm-password should be same");
+      setchPassData({ ...chpassData, newpass: "", confpass: "" });
+    } else {
+      try {
+        // Make an API request to your backend server
+        const response = await axios.get(
+          `http://localhost:8000/changePass/${email}`
+        );
+        const data = response.data;
+
+        if (data) {
+          const { email } = data;
+          console.log("email: " + email);
+          console.log("newpass: " + newpass);
+          // toast.loading("Loading...");
+          // setTimeout(() => {
+          //   toast.dismiss();
+          // }, 3000);
+          try {
+            const response = await axios.post(
+              "http://localhost:8000/changePass",
+              {
+                email: email,
+                newpass: newpass,
+              }
+            );
+            console.log(response.data); // Handle success response
+          } catch (error) {
+            console.error(error.message); // Handle error response
+          }
+        }
+        //password successfully changed
+        console.log("Password successfully changed");
+      } catch (error) {
+        console.error("Error retrieving salon data:", error.message);
+      }
+    }
+  };
+
   return (
-    <div className="desktop-3">
-      <div className="desktop-3-child" />
-      <b className="change-password">Change Password</b>
-      <input
-        className="password"
-        type="password"
-        placeholder="New Password"
-        maxLength={10}
-        minLength={8}
-      />
-      <input
-        className="confirm"
-        type="password"
-        placeholder="Confirm Password"
-        maxLength={10}
-        minLength={8}
-      />
-      <button className="setpass">
-        <b className="set-password">Set Password</b>
-      </button>
+    <div className="changepassflex">
+      <div className="changepasswidth">
+        <div className="changepassjustify">
+          <form action="" onSubmit={handleSubmit}>
+            <div className="changeinput">
+              <input
+                className="chpsin"
+                type="email"
+                value={chpassData.email}
+                onChange={handleChange}
+                placeholder="Email Address"
+                name="email"
+                required
+              />
+            </div>
+            <div className="changeinput">
+              <input
+                className="chpsin"
+                type="password"
+                value={chpassData.newpass}
+                onChange={handleChange}
+                placeholder="New Password"
+                name="newpass"
+                required
+              />
+            </div>
+            <div className="changeinput">
+              <input
+                className="chpsin"
+                type="password"
+                value={chpassData.confpass}
+                onChange={handleChange}
+                placeholder="Confirm Password"
+                name="confpass"
+                required
+              />
+            </div>
+            <div className="changebtn changeinput">
+              <button type="submit">Change Password</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
